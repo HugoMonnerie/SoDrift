b8e1b214a2923791436adab49d01b21dc464c76c
Hugo Monnerie <hugo.monnerie@edu.itescia.fr>
2022-06-14 13:52:36 +0200

    use wheel collider

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,63 +1,67 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Car : MonoBehaviour
-{
-    [SerializeField] float m_TranslationSpeed; // m/s
-    [SerializeField] float m_RotationSpeed; // °/s
+public class Car : MonoBehaviour {
+     
+    private float hInput;
+    private float vInput;
+    private float currentSteerAngle;
 
-    Rigidbody m_Rigidbody;
-
-    void Awake()
+    [SerializeField] private float motorForce;
+    [SerializeField] private float maxSteerAngle;
+    
+    [SerializeField] private WheelCollider wheelBackLeftCollider; 
+    [SerializeField] private WheelCollider wheelBackRightCollider; 
+    [SerializeField] private WheelCollider wheelFrontLeftCollider; 
+    [SerializeField] private WheelCollider wheelFrontRightCollider; 
+    
+    [SerializeField] private Transform wheelBackLeftTransform; 
+    [SerializeField] private Transform wheelBackRightTransform; 
+    [SerializeField] private Transform wheelFrontLeftTransform; 
+    [SerializeField] private Transform wheelFrontRightTransform; 
+    
+    public void FixedUpdate()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
     }
 
-    //COMPORTEMENT CINEMATIQUE
-    // Update is called once per frame
-    void Update()
+    private void GetInput()
     {
-        /*
-        float hInput = Input.GetAxisRaw("Horizontal");
-        float vInput = Input.GetAxis("Vertical");
-
-        //Vector3 moveVect = vInput*transform.forward * m_TranslationSpeed * Time.deltaTime;
-        //transform.Translate(moveVect, Space.World);
-
-        Vector3 localMoveVect = new Vector3(0, 0, vInput * m_TranslationSpeed * Time.deltaTime);
-        transform.Translate(localMoveVect, Space.Self);
-
-        float deltaAngle = hInput * m_RotationSpeed * Time.deltaTime;
-        transform.Rotate(transform.up, deltaAngle, Space.World);
-        */
+        hInput = Input.GetAxisRaw("Horizontal");
+        vInput = Input.GetAxis("Vertical");
     }
 
-    //COmportement dynamique (physique ... moteur PhysX)
-    private void FixedUpdate()
+    private void HandleMotor()
     {
-        float hInput = Input.GetAxisRaw("Horizontal");
-        float vInput = Input.GetAxis("Vertical");
+        wheelFrontLeftCollider.motorTorque = vInput * motorForce;
+        wheelFrontRightCollider.motorTorque = vInput * motorForce;
+    }
 
-        //MODE POSITIONNEL
-        /*
-        Vector3 moveVect = vInput * transform.forward * m_TranslationSpeed * Time.fixedDeltaTime;
-        m_Rigidbody.MovePosition(m_Rigidbody.position + moveVect);
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * hInput;
+        wheelFrontLeftCollider.steerAngle = currentSteerAngle;
+        wheelFrontRightCollider.steerAngle = currentSteerAngle;
+    }
 
-        float deltaAngle = hInput * m_RotationSpeed * Time.fixedDeltaTime;
-        Quaternion qRot = Quaternion.AngleAxis(deltaAngle, transform.up);
-        Quaternion qNewOrient = qRot * transform.rotation;
-        m_Rigidbody.MoveRotation(qNewOrient);
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(wheelFrontLeftCollider, wheelFrontLeftTransform);
+        UpdateSingleWheel(wheelFrontRightCollider, wheelFrontRightTransform);
+        UpdateSingleWheel(wheelBackLeftCollider, wheelBackLeftTransform);
+        UpdateSingleWheel(wheelBackRightCollider, wheelBackRightTransform);
+    }
 
-        m_Rigidbody.AddForce(-m_Rigidbody.velocity, ForceMode.VelocityChange);
-        m_Rigidbody.AddTorque(-m_Rigidbody.angularVelocity, ForceMode.VelocityChange);
-        */
-        //MODE VELOCITY
-        
-        Vector3 translationVelocity = vInput * transform.forward * m_TranslationSpeed;
-        m_Rigidbody.AddForce(translationVelocity-m_Rigidbody.velocity, ForceMode.VelocityChange);
-
-        Vector3 angularVelocity = hInput * transform.up * m_RotationSpeed * Mathf.Deg2Rad;
-        m_Rigidbody.AddTorque(angularVelocity-m_Rigidbody.angularVelocity , ForceMode.VelocityChange);
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
     }
 }
